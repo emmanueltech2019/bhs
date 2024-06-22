a47f05aa175ec21b8a67382ca809a732799fc497
EMMANUEL LUCKY <emmanueltech2019@gmail.com>
2024-06-22 15:19:06 +0100

    first commit

---
+++ src/app/data.tsx
@@ -0,0 +1,20 @@
+interface Data {
+    city: string;
+    state: string;
+    brand: string;
+    domain: string;
+    logo: string;
+    src: string;
+}
+
+const data:Data ={
+    brand:"BHS",
+    city:"Brooklyn",
+    state:"TX",
+    domain: "brooklynherniaspecialists.com",
+    logo: 'https://res.cloudinary.com/wise-solution-inc/image/upload/v1719065871/1Asset_71_lwcbuu.png',
+    src:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d96873.70582490717!2d-73.94499755!3d40.645244!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24416947c2109%3A0x82765c7404007886!2sBrooklyn%2C%20NY%2C%20USA!5e0!3m2!1sen!2sng!4v1719065485453!5m2!1sen!2sng"
+}
+
+
+export default data
