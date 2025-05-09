abc464b28b752a7cc9a019309d76eae0fe4e7e4f
Emmanuel Lucky <emmanueltech2019@gmail.com>
2025-05-09 06:42:25 +0100

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,23 +41,27 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <meta name="google-site-verification" content="SD8b8ohNyJ0kp_RMolMw9V3gNcwZ_26otqwcvSv8RAM" />
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-WW30H5PPC6"></script>
+      <meta name="google-site-verification" content="qtF6bh1d2Fs5IN5P9-UIymEDB64u8AuQyVBbWI6dNB8" />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-WYFS8C7VC0"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-WW30H5PPC6');
+              gtag('config', 'G-WYFS8C7VC0');
             `,
           }}
         />
       </head>
       <body className={inter.className}>
         <NavBar/>
+        <div className="overflow-x-hidden w-screen">
         {children}
+
+        </div>
         <TawkTo/>
+       
         <Footer/>
       </body>
     </html>
